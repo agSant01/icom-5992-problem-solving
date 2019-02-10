03135457a9a80a5dd4c5ec0dc7c529d45cd92abf
Gabriel Santiago <gabriel.santiago16@upr.edu>
2019-02-10 12:48:54 -0400

    Initial Commit

---
+++ PasswordGenerator/src/test/Test.java
@@ -0,0 +1,29 @@
+/**
+ * PasswordGenerator.test
+ * Test.java
+ * @date Feb 10, 2019
+ * @author Gabriel S. Santiago
+ */
+package test;
+
+import main.PasswordGenerator;
+
+/**
+ * @author Gabriel S. Santiago
+ * @date Feb 10, 2019
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		PasswordGenerator generator = new PasswordGenerator("agSav-nt01", "facebook.com");
+		generator.setInvalidCahracterRegex(",L-");
+		String pass = generator.generatePassword();
+		
+		print("Password: " + pass);
+		print("Length: " + pass.length());
+	}
+	
+	public static void print(Object o) {
+		System.out.println(o.toString());
+	}
+}
